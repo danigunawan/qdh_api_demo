aae9ec9900a4c2be68deec14592b6e092b81bfce
Lambda zhang <ggxxde@163.com>
2018-08-01 11:13:10 +0800

    upload_video

---
+++ nodejs/libs/qdh_api.js
@@ -0,0 +1,72 @@
+var http = require('http');
+var fs = require('fs');
+var Path = require('path');
+var DEBUG = false;
+
+function video_upload(host, port, filepath, description, cb) {
+    if (!fs.existsSync(filepath)) {
+        return cb && cb("not found file: " + filepath)
+    }
+    if(!host || !port || !description) {
+        return cb && cb("invalid Parameters")
+    }
+    var filename = Path.basename(filepath);
+    var stat = fs.statSync(filepath);
+
+    var options = {
+        host: host,
+        port: port,
+        method: "POST",
+        path: "/api/lambda/pk/upload_movies/"
+    }
+
+    var req = http.request(options, function(res){
+        res.on("data", function(chunk){
+            DEBUG && console.log("BODY:" + chunk);
+        })
+    })
+    req.on('error', function(e){
+        return cb && cb('problem with request:' + e.message)
+    })
+
+    var boundaryKey = Math.random().toString(16);
+    var enddata = '\r\n----' + boundaryKey + '--';
+    var content = "\r\n----" + boundaryKey + "\r\n" +
+                  "Content-Type: application/octet-stream\r\n" +
+                  '----' + boundaryKey + '\r\n' +
+                  'Content-Disposition: form-data; name="name"\r\n\r\n' +
+                  description +
+                  '----' + boundaryKey + '\r\n' +
+                  'Content-Disposition: form-data; name="scene"\r\n\r\n'+
+                  '0' +
+                  '----' + boundaryKey + '\r\n' +
+                  'Content-Disposition: form-data; name="nth"\r\n\r\n' +
+                  '11' +
+                  '----' + boundaryKey + '\r\n' +
+                  'Content-Disposition: form-data; name="rescale"\r\n\r\n' +
+                  '11' +
+                  '----'+ boundaryKey + '\r\n' +
+                  'Content-Disposition: form-data; name="csrfmiddlewaretoken"\r\n\r\n' +
+                  'tBqqvVtb3EfgO95vr7jKQ5IEBSD4FAaPU0TMHFtJtyb68T7lHelgR38nIwSGLrrU' +
+                  '----' + boundaryKey + '\r\n' +
+                  'Content-Disposition: form-data; name="file"; filename=' + filename + '\r\n' +
+                  'Content-Type: video/mp4\r\n\r\n';
+    var contentBinary = new Buffer(content, 'utf-8');//当编码为ascii时，中文会乱码。
+
+    var contentLength = contentBinary.length + stat.size;
+
+    req.setHeader('Content-Type', 'multipart/form-data; boundary=--' + boundaryKey);
+    req.setHeader('Content-Length', contentLength + Buffer.byteLength(enddata));
+
+    req.write(contentBinary);
+    var fileStream = fs.createReadStream(filepath, {bufferSize : 4 * 1024});
+    fileStream.pipe(req, {end: false});
+    fileStream.on('end', function() {
+        req.end(enddata);
+        return cb && cb(null)
+    });
+}
+
+module.exports = {
+  video_upload : video_upload
+}
--- nodejs/main.js
+++ nodejs/main.js
@@ -1,58 +1,12 @@
-var http = require('http');
-var fs = require('fs');
-var fs = require('path');
+var api = require('./libs/qdh_api');
 
-function video_upload(host, port, path, filepath, cb) {
-    if(!host || !port || !path) {
-        return cb && cb("invalid Parameters")
-    }
-
-   var filename = path.basename(filepath);
-
-    var boundaryKey = '----' + new Date().getTime();    // 用于标识请求数据段
-    var options = {
-        host: host,
-        port: port,
-        method: 'POST',
-        path: path,
-        headers: {
-            'Content-Type': 'multipart/form-data; boundary=' + boundaryKey,
-            'Connection': 'keep-alive'
-        }
-    };
-    var req = http.request(options, function(res){
-        res.setEncoding('utf8');
-
-        res.on('data', function(chunk) {
-            console.log('body: ' + chunk);
-        });
-
-        res.on('end', function() {
-            console.log('res end.');
-        });
-    });
-    /*req.write(
-         '--' + boundaryKey + 'rn' +
-         'Content-Disposition: form-data; name="upload"; filename="test.txt"rn' +
-         'Content-Type: text/plain'
-     );*/
-    req.write(
-        `--${boundaryKey}rn Content-Disposition: form-data; name="${self.path}"; filename="${self.file}"rn Content-Type: text/plain`
-    );
-
-    // 创建一个读取操作的数据流
-    let fileStream = fs.createReadStream(filepath);
-    fileStream.pipe(req, {end: false});
-    fileStream.on('end', function() {
-        req.end('rn--' + boundaryKey + '--');
-        return cb&& cb(null);
-    });
-}
-
-
-var host = '192.168.3.8'
+var host = "192.168.3.8";
 var port = 8000;
-var path = '/api/lambda/pk/upload_movies/'
-video_upload(host, port, path, 'citest.mp4', function(err) {
 
-});
+console.log(">>> uploading video");
+api.video_upload(host, port, "../citest.mp4", "测试视频2", function(err) {
+    if(err) {
+        return console.log(err)
+    }
+    console.log(">>> uploaded");
+})
