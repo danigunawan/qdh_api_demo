2078bd537de170b602b5d046506509ca7aca040d
rick0626 <rick.li_@outlook.com>
2018-08-08 18:06:25 +0800

    add image search result

--- python/req_test.py
+++ python/req_test.py
@@ -1,50 +1,111 @@
 # -*- coding: utf-8 -*-
 # @Author: li
 # @Date:   2018-08-01 09:36:14
-# @Last Modified by:   li
-# @Last Modified time: 2018-08-02 11:39:15
+# @Last Modified by:   li_coder
+# @Last Modified time: 2018-08-08 17:15:56
+import os
 import requests
 import base64
+from lxml import etree
+import time
 
-BASE_URL = "http://192.168.31.153:8000"
+class APItest():
+	def __init__(self, host, port):
 
-def video_upload():
-	url = BASE_URL + "/api/lambda/pk/upload_movies/"
-	files = {"file": open("../test.mp4", "rb")}
-	data = {
-		"name": "test",
-		# "scene": True,
-		"nth": 1
-	}
+		self.host = host
+		self.port = port
+		self.headers = {
+			"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"
+		}
+		self.session = requests.session()
 
-	response = requests.post(url, files=files, data=data)
-	return response.text
+		rep = self.session.get("http://{}:{}/".format(self.host, self.port), headers=self.headers)
+		html = etree.HTML(rep.text)
+		self.csrftoken = html.xpath('//input[@name="csrfmiddlewaretoken"]/@value')[0]
+		# print(csrftoken)
+		data = {
+			"username": "admin", 
+			"password": "super",
+			"csrfmiddlewaretoken": self.csrftoken
+		}
+		self.session.post("http://{}:{}/accounts/login/".format(self.host, self.port), data=data)
 
-def image_search():
-	img = open("../test.png", "rb")
-	img_base64 = base64.b64encode(img.read())
-	img.close()
+	def video_upload(self, filepath, description):
+		if not all([filepath, description]):
+			return "invalid Parameters"
 
-	print(type(img_base64))
+		if not os.path.exists(filepath):
+			return "not found file: {}".format(filepath)
 
-	data = {
-		'image_url': "data:image/png;base64,".encode() + img_base64,
-		'count': 20,
-		'selected_indexers':'["2_1"]',
-		'selected_detectors':'["1"]',
-		'generate_tags':"false",
-		'csrfmiddlewaretoken':'9OyeJGolnVMywTMSvnAIrZdAZWY14PdRf7BolLDFLc6mWrYPny4N0AxMeCfl0exq'
-	}
+		url = "http://{}:{}".format(self.host, self.port) + "/api/lambda/pk/upload_movies/"
+		# files = {"file": open("../test.mp4", "rb")}
+		files = {"file": open(filepath, "rb")}
+		data = {
+			"name": description,
+			# "scene": True,
+			"nth": 1,
+			# "csrfmiddlewaretoken": self.csrftoken
+		}
 
-	url = BASE_URL + "/Search2"
-	response = requests.post(url, data=data)
-	return response.text
+		response = requests.post(url, files=files, data=data)
+		return response.text
+
+	def image_search(self, filepath):
+		if not all([filepath, ]):
+			return "invalid Parameters"
+
+		if not os.path.exists(filepath):
+			return "not found file: {}".format(filepath)
+
+		# img = open("../test.png", "rb")
+		img = open(filepath, "rb")
+		img_base64 = base64.b64encode(img.read())
+		img.close()
+
+		# print(type(img_base64))
+
+		data = {
+			'image_url': "data:image/png;base64,".encode() + img_base64,
+			'count': 20,
+			'selected_indexers':'["2_1"]',
+			'selected_detectors':'["1"]',
+			'generate_tags':"false",
+			'csrfmiddlewaretoken':self.csrftoken
+		}
+
+		url = "http://{}:{}".format(self.host, self.port) + "/Search2"
+		response = self.session.post(url, data=data)
+		return response.text
+
+	def image_search_result_all(self):
+		url = "http://{}:{}".format(self.host, self.port) + "/api/queriesset/?format=json"
+		response = self.session.get(url)
+		return response.text
+
+	def image_search_result(self, id):
+		url = "http://{}:{}/api/lambda/{}/queriesresults/?format=json".format(self.host, self.port, id)
+		response = self.session.get(url)
+		return response.text
+
 
 if __name__ == "__main__":
-	# ret = video_upload()
-	# print(ret)
 
-	search_resutl = image_search()
-	page = open("list.html", "w")
-	page.write(search_resutl)
-	page.close()
+	host = "192.168.31.153"
+	port = 8000
+	
+	api_test = APItest(host, port)
+
+	ret = api_test.video_upload("../test.mp4", "api_test1")
+	print(ret)
+
+	ret = api_test.image_search("../test.png")
+	print(ret)
+
+	ret = api_test.image_search_result_all()
+	print(ret)
+
+	ret = api_test.image_search_result(1)
+	print(ret)
+
+
+
